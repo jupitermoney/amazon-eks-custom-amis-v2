41586f87fb5e1c7cb45c2bb58c617cfc762c4fbd
Ashwani Singh <ashwani.singh@opstree.com>
2023-08-08 14:54:38 +0530

    kernel upgrade

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,7 +7,6 @@ set -o errexit
 # upgrade the operating system
 yum update -y && yum autoremove -y
 yum install -y parted system-lsb-core
-yum update -y kernel
 
 # enable the epel release
 amazon-linux-extras install epel -y
